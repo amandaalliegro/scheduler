8444934ee40fceecb375ebfa23f873ceee635db7
Amanda Alliegro <68923553+amandaalliegro@users.noreply.github.com>
2020-10-27 18:57:33 -0400

    Refactoring Interviewer Components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+import InterviewerListItem from 'components/InterviewerListItem';
+
+export default function InterviewerList (props) {
+  const interviewersOptions = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewersOptions}
+      </ul>
+    </section>
+  )
+}
