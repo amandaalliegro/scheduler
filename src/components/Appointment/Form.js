c0e7bfedc045014796b68b6311009cc471a3418e
Amanda Alliegro <68923553+amandaalliegro@users.noreply.github.com>
2020-10-28 07:49:49 -0400

    adding state to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,29 +4,39 @@ import Button from 'components/Button'
 
 const Form = props => {
 
-  const [name, setName] = useState("")
-  const [interviewer, setInterviewer] = useState(null)
+const [name, setName] = useState(props.name || "");
+const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+const reset = () => {
+  setName("")
+  setInterviewer(null)
+}
+
+const cancel = () => {
+  reset();
+  props.onCancel();
+}
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
             value={props.name}
-          // onChange={handleInput}
           />
         </form>
+
         <InterviewerList
           interviewers={props.interviewers}
           interviewer={props.interviewer}
           setInterviewer={setInterviewer}
         />
       </section>
+    
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={props.onCancel}>Cancel</Button>
