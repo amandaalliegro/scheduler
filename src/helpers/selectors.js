d47ca714a6c9bd684bad9142683665a0e99a32c1
Amanda Alliegro <68923553+amandaalliegro@users.noreply.github.com>
2020-10-28 21:34:00 -0400

    Transforming Data with Selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,5 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(elem => elem.name === day)[0]
+  const appointments = filteredDay ? filteredDay.appointments.map(elem => state.appointments[elem]) : [];
+  return appointments;
+} 
