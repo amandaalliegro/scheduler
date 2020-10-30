cf5eec95631683a523afd0cd3fdae9e921174ec7
Amanda Alliegro <68923553+amandaalliegro@users.noreply.github.com>
2020-10-29 20:20:35 -0400

    Building a Hook

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,3 +3,12 @@ export function getAppointmentsForDay(state, day) {
   const appointments = filteredDay ? filteredDay.appointments.map(elem => state.appointments[elem]) : [];
   return appointments;
 } 
+
+export function getInterview(state, interview) {
+  if (interview) {
+    const newInterview = { ...interview };
+    newInterview.interviewer = state.interviewers[newInterview.interviewer]
+    return newInterview;
+  }
+  return null;
+}
